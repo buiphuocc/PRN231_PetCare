0a2e6154da14e9616fa9098ef33d18846dadb7b3
SameyoRoi <nguyenkyanhminh@gmail.com>
2024-10-01 19:06:03 +0700

    update

--- PRN231_PetCare/Program.cs
+++ PRN231_PetCare/Program.cs
@@ -2,7 +2,6 @@ using Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -20,7 +19,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
